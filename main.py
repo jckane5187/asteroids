4865cca16a26e10b273c05736a9024422d6f60aa
jckane5187 <jckane5187@gmail.com>
2026-01-01 14:38:38 -0600

    updated main to set clock speed at 60 fps and added a dt variable that updates with clock speed

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +19,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60) / 1000)
+        print(dt)
 
 
 if __name__ == "__main__":
