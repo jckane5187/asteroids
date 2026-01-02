6a53fd982b2026e1b2492260ed19cdfaa2197069
jckane5187 <jckane5187@gmail.com>
2026-01-02 11:34:04 -0600

    updated main to include two groups: updatable and drawable, added Player to both groups, updated player.py update method to include the directional keys

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,8 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for ob in drawable:
+            ob.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60) / 1000)
