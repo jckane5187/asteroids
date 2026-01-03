479f4a13935996191be84f7c061ec371ea690671
jckane5187 <jckane5187@gmail.com>
2026-01-03 16:04:39 -0600

    added collision method to CircleShape, added collision detection to main, added event log to main, updated gitignore to ignore event logger

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        distance_between = pygame.math.Vector2.distance_to(self.position, other.position)
+        collision_distance = self.radius + other.radius
+
+        return distance_between <= collision_distance
