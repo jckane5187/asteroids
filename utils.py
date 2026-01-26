1ed78f280b29747a9f891ea74de62303e6dd6ebf
jckane5187 <jckane5187@gmail.com>
2026-01-26 15:54:35 -0500

    added utils.py for utility fucntions, created a basic wrapping function

---
+++ utils.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def position_wrap(x, y):
+    if x < 0:
+        x = SCREEN_WIDTH
+    if x > SCREEN_WIDTH:
+        x = 0
+    if y > SCREEN_HEIGHT:
+        y = 0
+    if y < 0:
+        y = SCREEN_HEIGHT   
