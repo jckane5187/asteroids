0dc0a280152400323e497f029f401646b014004a
jckane5187 <jckane5187@gmail.com>
2026-01-26 16:23:33 -0500

    updated constants with a asteroid wrap timer and updated asteroid class with new attribute (self.wrap_timer)and updated logic to prevent the asteroids from instantly wrapping after spawn (was causing strange flickering effect)

--- asteroid.py
+++ asteroid.py
@@ -1,20 +1,23 @@
 import pygame
 import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_WRAP_SPAWN_TIMER
 from logger import log_event
 from utils import position_wrap
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.wrap_timer = ASTEROID_WRAP_SPAWN_TIMER
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
+        self.wrap_timer -= dt
         self.position += self.velocity * dt
-        self.position.x, self.position.y = position_wrap(self.position.x, self.position.y)
+        if self.wrap_timer < 0:
+            self.position.x, self.position.y = position_wrap(self.position.x, self.position.y)
 
     def asteroid_split(self):
         self.kill()
