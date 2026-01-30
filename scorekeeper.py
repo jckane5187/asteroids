bea56934623958ec94e7d92b53379b2a5fecc0af
jckane5187 <jckane5187@gmail.com>
2026-01-30 11:19:04 -0600

    added multi_surface to main to display current multiplier, removed print debugging statement from consecutive_multi_increase method

--- scorekeeper.py
+++ scorekeeper.py
@@ -11,7 +11,6 @@ class Scoreboard():
     def consecutive_multi_increase(self, radius):
         potential_new_multiplier = self.consecutive_multi + (BASE_CONSEC_SHOT_MULTI_INC * (((ASTEROID_MAX_RADIUS - radius) // ASTEROID_MIN_RADIUS) + 1))
         self.consecutive_multi = min(potential_new_multiplier, MAX_CONSEC_SHOT_MULTI)
-        print(self.consecutive_multi)
 
     def reset_consecutive_multi(self):
         self.consecutive_multi = 1
